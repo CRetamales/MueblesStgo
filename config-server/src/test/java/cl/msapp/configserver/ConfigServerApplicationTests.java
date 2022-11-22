038550aada71eb1acff0268ed0a15d2ff9b4b894
Carlos Retamales <carlos.retamales.a@usach.cl>
2022-11-21 23:23:18 -0300

    [Carlos Retamales] - Migración de Gradle a Maven

---
+++ config-server/src/test/java/cl/msapp/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.msapp.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
