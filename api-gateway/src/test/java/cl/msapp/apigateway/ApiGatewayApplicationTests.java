a58c1ad240f06b07ab0b6d008517de609f1405b2
Carlos Retamales <carlos.retamales.a@usach.cl>
2022-11-22 00:12:29 -0300

    [Carlos Retamales] - Migración de Gradle a Maven

---
+++ api-gateway/src/test/java/cl/msapp/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.msapp.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
